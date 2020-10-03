cdceb86193702bb3e1fde1648f9020219780a166
Vincent Wiart <vwiart@users.noreply.github.com>
2020-10-03 10:59:23 +0200

    adding comments (#4)

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,21 @@ struct Kafka {
 
 impl Kafka {
     fn list_topics(&self) {
+        // creating the consumer
         let consumer: BaseConsumer = ClientConfig::new()
             .set("bootstrap.servers", self.url)
             .create()
             .expect("Consumer creation failed");
 
+        // read metadata.
         let metadata = consumer.fetch_metadata(None, self.timeout)
             .expect("Failed to fetch metadata");
 
+        // extrac topics.
         let topics = metadata.topics();
 
         println!("  Topics count: {}", topics.len());
+        // let's loop over the topics and print them out.
         for topic in topics {
             println!("\t{}", topic.name());
         }
@@ -32,5 +36,6 @@ fn main() {
     let timeout: Duration = Duration::new(5, 0);
 
     let kafka = Kafka { url: kafka_url, timeout: timeout };
+    // try and list topics.
     kafka.list_topics();
 }
